c3fb49cdbc2e4dc8567306c1f7f9959667a53752
Sbusiso17 <152275698+Sbusiso17@users.noreply.github.com>
2024-07-10 10:49:49 +0200

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,4 @@
-def call{}{
-
- steps {
-         echo "checking out at our code"
-         git branch: 'main', url: 'https://github.com/Sbusiso17/Petclinic-tomcat-deploy.git'
-            }
-
-} 
+def call() {
+    echo "Checking out our code"
+    git branch: 'main', url: 'https://github.com/Sbusiso17/Petclinic-tomcat-deploy.git'
+}
